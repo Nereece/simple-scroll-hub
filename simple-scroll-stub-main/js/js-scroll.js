8cbf421654705113c4a5129f1dc87553a13f68c4
Nereece <115160696+Nereece@users.noreply.github.com>
2022-11-01 12:23:47 +0000

    confusing js scroll stuff

---
+++ simple-scroll-stub-main/js/js-scroll.js
@@ -0,0 +1,62 @@
+const scrollElements = document.querySelectorAll(".js-scroll");
+scrollElements.forEach((el) => {
+  if (elementInView(el)) {
+    console.log("displayElements")
+    displayScrollElement(el)
+  }
+}
+
+)
+// display functions, el means element 
+// checks whether element is in viewable area 
+function elementInView(el) {
+  const elementTop = el.getBoundingClientRect().top;
+  const elementHeight = el.getBoundingClientRect().height;
+  console.log(elementTop, elementHeight)
+  return (
+    elementTop <= document.documentElement.clientHeight && elementTop > 0
+  )
+}
+// checks whether element is above or below viewable area 
+function elementOutOfView(el) {
+  const elementTop = el.getBoundingClientRect().top;
+  const elementBottom = el.getBoundingClientRect().bottom;
+  return (
+    elementTop >= document.documentElement.clientHeight || elementBottom < 0
+  )
+}
+
+// show element 
+function displayScrollElement(el) {
+  el.classList.add("scrolled");
+}
+
+// hide scroll element 
+function hideScrollElement(el) {
+  el.classList.remove("scrolled");
+}
+
+//UTILITY
+// throttle - fn = function to call, wait = interval in ms
+function throttle(fn, wait) {
+  let inThrottle, lastFn, lastTime;
+  return function () {
+    const context = this,
+      args = arguments;
+    if (!inThrottle) {
+      fn.apply(context, args);
+      lastTime = Date.now();
+      inThrottle = true;
+    } else {
+      clearTimeout(lastFn);
+      lastFn = setTimeout(function () {
+        if (Date.now() - lastTime >= wait) {
+          fn.apply(context, args);
+          lastTime = Date.now();
+        }
+      }, Math.max(wait - (Date.now() - lastTime), 0));
+    }
+  };
+};
+
+
